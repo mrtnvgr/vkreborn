ac8727be24a5722d0b6dc626d11a36ee0dff61f4
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-01 22:40:53 +0700

    add loguru

--- vkreborn/__main__.py
+++ vkreborn/__main__.py
@@ -1,14 +1,20 @@
 import os
 
+from loguru import logger
 from vkbottle import Bot
 
 from .handlers import labelers
 
-if __name__ == "__main__":
 
+@logger.catch
+def main():
     bot = Bot(os.environ["VKTOKEN"])
 
     for labeler in labelers:
         bot.labeler.load(labeler)
 
     bot.run_forever()
+
+
+if __name__ == "__main__":
+    main()

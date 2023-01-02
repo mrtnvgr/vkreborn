3bcdcea0ebb2f161fb270d0f2d3ad36142fdf596
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-03 00:14:26 +0700

    db models: add user model

---
+++ vkreborn/database/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Boolean,
+)
+
+from vkreborn.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, unique=True, nullable=False)
+    is_admin = Column(Boolean, nullable=False)
+
+    def __repr__(self):
+        return f"<User: {self.user_id}>"

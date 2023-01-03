e1e421774127d3835d3ef9d849b86589a5a0a101
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-03 22:26:14 +0700

    roll cmd: add def without args

--- vkreborn/handlers/roll.py
+++ vkreborn/handlers/roll.py
@@ -5,6 +5,12 @@ import random
 labeler = UserLabeler()
 
 
+@labeler.message(command="roll")
+async def roll_default_handler(message: Message):
+    response = random.randint(1, 100)
+    return await message.reply(response)
+
+
 @labeler.message(CommandRule("roll", args_count=1), text="<_> <start:int>-<end:int>")
 async def roll_ints_handler(message: Message, start: int, end: int):
     response = random.randint(start, end)

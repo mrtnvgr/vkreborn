1c5380329f1e814010f0b4e5fccf39d8585b621d
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-03 22:42:53 +0700

    roll cmd: add list def

--- vkreborn/handlers/roll.py
+++ vkreborn/handlers/roll.py
@@ -1,6 +1,16 @@
 from vkbottle.user import Message, UserLabeler
-from vkbottle.dispatch.rules.base import CommandRule
+from vkbottle.dispatch.rules.base import VBMLRule
+from vbml import Patcher
+import shlex
 import random
+
+patcher = Patcher()
+
+
+@patcher.validator("list")
+def list_validator(value: str):
+    return shlex.split(value, posix=True)
+
 
 labeler = UserLabeler()
 
@@ -11,7 +21,18 @@ async def roll_default_handler(message: Message):
     return await message.reply(response)
 
 
-@labeler.message(CommandRule("roll", args_count=1), text="<_> <start:int>-<end:int>")
+@labeler.message(text="<_>roll <start:int>-<end:int>")
 async def roll_ints_handler(message: Message, start: int, end: int):
     response = random.randint(start, end)
     return await message.reply(response)
+
+
+@labeler.message(VBMLRule("<_>roll <args:list>", patcher=patcher))
+async def roll_strings_handler(message: Message, args: list):
+    choice = random.choice(args)
+    return await message.reply(cleanup(choice))
+
+
+def cleanup(value: str):
+    value = value.removesuffix(",")
+    return value

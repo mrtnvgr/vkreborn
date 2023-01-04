23a121f969baef0282cbd752b562438c63a7cae2
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-04 09:51:14 +0700

    fix wrong func name

--- vkreborn/handlers/whoami.py
+++ vkreborn/handlers/whoami.py
@@ -4,7 +4,7 @@ from vkreborn.repositories import UserRepository
 
 
 @labeler.message(command="whoami")
-async def new_user_handler(message: Message):
+async def whoami_handler(message: Message):
     repo = UserRepository(user_id=message.from_id)
     user_info = await repo.get_user()
     response = "Админ" if user_info.is_admin else "Пользователь"

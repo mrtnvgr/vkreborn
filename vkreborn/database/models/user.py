63f6dd702e403a774e17da56e40b522209486e84
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-04 09:46:56 +0700

    user db model: add muted_until

--- vkreborn/database/models/user.py
+++ vkreborn/database/models/user.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     Column,
     Integer,
     Boolean,
+    DateTime,
 )
 
 from vkreborn.database import Base
@@ -12,7 +13,8 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, unique=True, nullable=False)
-    is_admin = Column(Boolean, nullable=False)
+    is_admin = Column(Boolean, default=False, nullable=False)
+    muted_until = Column(DateTime, default=None)
 
     def __repr__(self):
         return f"<User: {self.user_id}>"
--- vkreborn/repositories/user.py
+++ vkreborn/repositories/user.py
@@ -1,9 +1,8 @@
 from typing import Optional
-
-from sqlalchemy.sql import insert, select
-
+from sqlalchemy.sql import insert, select, update
 from vkreborn.database import engine
 from vkreborn.database.models import User
+from datetime import datetime
 
 
 class UserRepository:
@@ -20,10 +19,16 @@ class UserRepository:
         async with engine.connect() as conn:
             query = insert(User).values(user_id=self.user_id, is_admin=is_admin)
             await conn.execute(query)
-            await conn.commit()
 
     async def get_admin_ids(self):
         async with engine.connect() as conn:
             query = select(User.user_id).where(User.is_admin)
             admins: User = (await conn.execute(query)).fetchall()
             return [admin[0] for admin in admins]
+
+    async def mute_user_until(self, date: Optional[datetime | None]):
+        async with engine.connect() as conn:
+            query = (
+                update(User).where(User.user_id == self.user_id).value(muted_until=date)
+            )
+            await conn.execute(query)

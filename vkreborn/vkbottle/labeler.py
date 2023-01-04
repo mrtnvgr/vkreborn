e416b119d937a0044c3dc4987492c54b86027930
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-04 09:14:32 +0700

    add admin rule to custom labeler

--- vkreborn/vkbottle/labeler.py
+++ vkreborn/vkbottle/labeler.py
@@ -1,5 +1,7 @@
 from vkbottle.user import UserLabeler
+from vkreborn.rules import AdminRule
 from vkreborn.vbml import patcher
 
 labeler = UserLabeler()
 labeler.vbml_patcher = patcher
+labeler.custom_rules["admin"] = AdminRule()

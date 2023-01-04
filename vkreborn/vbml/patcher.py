28403d3a292d1482a8b04e82be89b0a8f1678267
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-04 09:36:45 +0700

    vbml patcher: add mention validator

--- vkreborn/vbml/patcher.py
+++ vkreborn/vbml/patcher.py
@@ -1,5 +1,6 @@
 from vbml import Patcher
 import shlex
+import re
 
 patcher = Patcher()
 
@@ -7,3 +8,14 @@ patcher = Patcher()
 @patcher.validator("list")
 def list_validator(value: str):
     return shlex.split(value, posix=True)
+
+
+@patcher.validator("mention")
+def mention_validator(value: str):
+    pattern = re.compile(r"^\[id[0-9]+\|@[A-Za-z0-9]+]$", re.IGNORECASE)
+    if pattern.match(value):
+        value = value.split("|")
+        return {
+            "id": value[0].removeprefix("["),
+            "domain": value[1].removesuffix("]"),
+        }

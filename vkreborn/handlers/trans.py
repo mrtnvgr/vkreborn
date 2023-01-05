57a89c9218854765ce21001381f26eb2d391a137
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-06 01:02:48 +0700

    trans cmd: strip output

--- vkreborn/handlers/trans.py
+++ vkreborn/handlers/trans.py
@@ -29,4 +29,4 @@ async def translate(content: str, to: str = "ru"):
     except TransError as ex:
         return f"Произошла неожиданная ошибка!\n{ex.status_code}: {ex.reason}"
 
-    return f'Перевод: "{response}"'
+    return f'Перевод: "{response.strip()}"'

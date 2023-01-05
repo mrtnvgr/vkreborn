56b5c171ac3db5eeaeca7b6ed560b90492454d10
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-06 00:44:33 +0700

    update .env.example

--- vkreborn/handlers/__init__.py
+++ vkreborn/handlers/__init__.py
@@ -1,12 +1,13 @@
-from vkreborn.handlers import new_user, fixlayout, whoami, admins, roll, mute
+from vkreborn.handlers import new_user, fixlayout, whoami, admins, roll, mute, trans
 
 labelers = {
     new_user.labeler,
+    mute.labeler,
     fixlayout.labeler,
     whoami.labeler,
     admins.labeler,
     roll.labeler,
-    mute.labeler,
+    trans.labeler,
 }
 
 __all__ = ["labelers"]

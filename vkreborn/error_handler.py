d5896937d4026d4c389aa6a1ef7dddd53b833abd
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-07 20:57:44 +0700

    ignore unk args in error handler

--- vkreborn/error_handler.py
+++ vkreborn/error_handler.py
@@ -6,7 +6,7 @@ error_handler = ErrorHandler(redirect_arguments=True)
 
 
 @error_handler.register_undefined_error_handler
-async def undefined_handler(e: BaseException, message: Message):
+async def undefined_handler(e: BaseException, message: Message, **_):
     try:
         await message.reply("Что-то пошло не так, произошла неожиданная ошибка!")
     except Exception as ex:

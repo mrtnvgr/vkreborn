c0b685be9d08266aa6d2ba3fa4d3986ad1e548d9
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-10 14:13:16 +0700

    remove muted from mute handler

--- vkreborn/handlers/for_admins/mute.py
+++ vkreborn/handlers/for_admins/mute.py
@@ -1,28 +1,8 @@
 from vkbottle.user import Message
 from vkreborn.vkbottle import labeler
 from vkreborn.repositories import UserRepository, MutedUserRepository
-from vkbottle.exception_factory import VKAPIError
 from vkreborn.error_handler import error_handler
 from datetime import datetime, timedelta
-from loguru import logger
-
-
-@labeler.chat_message(muted=True)
-@error_handler.catch
-async def muted_user_handler(message: Message):
-    repo = MutedUserRepository(user_id=message.from_id, muted_where=message.chat_id)
-    user = await repo.get()
-    if user.muted_until < datetime.now():
-        return await repo.delete()
-
-    try:
-        await message.ctx_api.messages.delete(
-            peer_id=message.peer_id,
-            cmids=message.conversation_message_id,
-            delete_for_all=True,
-        )
-    except VKAPIError[15] as ex:
-        logger.debug(ex.description)
 
 
 @labeler.chat_message(text="<_:prefix>mute <user:mention> <minutes:float>", admin=True)

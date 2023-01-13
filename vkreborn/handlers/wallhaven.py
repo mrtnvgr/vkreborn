5443bcb3d4efeaa712e2bf4468682462a768f967
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-13 21:48:07 +0700

    some work

--- vkreborn/handlers/wallhaven.py
+++ vkreborn/handlers/wallhaven.py
@@ -1,14 +1,40 @@
 from vkbottle.user import Message
 from vkreborn.vkbottle import labeler
 from vkreborn.error_handler import error_handler
+from vkbottle import PhotoMessageUploader
+from vkbottle.http import AiohttpClient
+import random
 
 
 @labeler.message(text="<_:prefix>wh", blocking=False)
 @error_handler.catch
 async def wh_noargs_handler(message: Message):
-    attachment = await get_picture()
+    photo = await get_random_picture()
+    attachment = await PhotoMessageUploader(message.ctx_api).upload(
+        photo, message.peer_id
+    )
     return await message.reply(attachment=attachment)
 
 
-async def get_picture(q: str = "", categories: str = "", purity: str = "100"):
-    pass
+async def get_random_picture(**kwargs):
+    seed = random.randint(1, 999999)
+    search = await wh_search(seed=seed, **kwargs)
+    picture = random.choice(search)
+    return picture["path"]
+
+
+async def wh_search(
+    q: str = "", categories: str = "111", purity: str = "100", seed: str = ""
+):
+    url = "https://wallhaven.cc/api/v1/search"
+    client = AiohttpClient()
+
+    payload = {
+        "q": q,
+        "categories": categories,
+        "purity": purity,
+        "seed": seed,
+    }
+
+    content = await client.request_json(url, data=payload)
+    return content

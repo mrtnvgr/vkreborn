2c7dbbba93c83c012792e70f176c017271a74f4e
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-15 09:57:16 +0700

    change whreset def name

--- vkreborn/handlers/for_admins/whreset.py
+++ vkreborn/handlers/for_admins/whreset.py
@@ -6,7 +6,7 @@ from vkreborn.error_handler import error_handler
 
 @labeler.chat_message(text="<_:prefix>whreset", admin=True)
 @error_handler.catch
-async def giveadmin_handler(message: Message):
+async def whreset_handler(message: Message):
     repo = WHPictureRepository(where_id=message.chat_id)
     await repo.reset_chat()
     return await message.reply("Кэш комманды wh очищен")

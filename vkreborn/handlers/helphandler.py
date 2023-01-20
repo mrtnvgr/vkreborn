e3e3416e45c1b7e74f6d5f8703ec744aa83521cd
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-20 19:00:52 +0700

    change url in help handler

--- vkreborn/handlers/helphandler.py
+++ vkreborn/handlers/helphandler.py
@@ -6,5 +6,5 @@ from vkreborn.error_handler import error_handler
 @labeler.message(text="<_:prefix>help", blocking=False)
 @error_handler.catch
 async def help_handler(message: Message):
-    url = "https://github.com/mrtnvgr/vkreborn/blob/main/CMDS.md"
+    url = "https://github.com/mrtnvgr/vkreborn/blob/main/docs/index.md"
     return await message.reply(url)

add4e53abaa40a5a5e5e9c84acb96f293e4d4593
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-21 10:34:19 +0700

    add forwarded attachments support

--- vkreborn/tools.py
+++ vkreborn/tools.py
@@ -4,10 +4,24 @@ from vkbottle.http import AiohttpClient
 
 async def get_attachments(message: Message):
     # NOTE: Check FAQ
+    attachments = await _get_attachments(message)
+    if attachments:
+        return attachments
+
+    if message.fwd_messages:
+        attachments = []
+        for msg in message.fwd_messages:
+            attachments.extend(await _get_attachments(msg))
+        return attachments
+    return []
+
+
+async def _get_attachments(message: Message):
     if message.attachments:
         return message.attachments
     if message.reply_message and message.reply_message.attachments:
         return message.reply_message.attachments
+    return []
 
 
 async def get_url_bytes(url: str):

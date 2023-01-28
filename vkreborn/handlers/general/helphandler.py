033d5c027b20c4e0c4e688b0f5e6f2ff75d28a0d
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-28 19:27:27 +0700

    help command: add more aliases

--- vkreborn/handlers/general/helphandler.py
+++ vkreborn/handlers/general/helphandler.py
@@ -5,6 +5,8 @@ from vkreborn.vkbottle import labeler
 
 
 @labeler.message(text="<_:prefix>help", blocking=False)
+@labeler.message(text="<_:prefix>хэлп", blocking=False)
+@labeler.message(text="<_:prefix>хелп", blocking=False)
 @error_handler.catch
 async def help_handler(message: Message):
     url = "https://github.com/mrtnvgr/vkreborn/blob/main/docs/index.md"

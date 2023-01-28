9990c7527570596ca460e15d9431ccdb3dd1d28d
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-28 21:20:11 +0700

    bass fx command: add more handlers

--- vkreborn/handlers/general/fx/bass.py
+++ vkreborn/handlers/general/fx/bass.py
@@ -9,8 +9,16 @@ defaults = {"attachment": SUPPORTED_ATTACHMENTS, "blocking": False}
 
 
 @labeler.message(text="<_:prefix>bassboost <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>бассбуст <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>басбуст <gain:gain>", **defaults)
 @labeler.message(text="<_:prefix>basscut <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>басскат <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>баскат <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>басскут <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>баскут <gain:gain>", **defaults)
 @labeler.message(text="<_:prefix>bass <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>басс <gain:gain>", **defaults)
+@labeler.message(text="<_:prefix>бас <gain:gain>", **defaults)
 @error_handler.catch
 async def bassboost_handler(message: Message, gain: float):
     return await make(message, BassEffect(gain=gain))

e50fbfb1cf805a26dc1aaa8deec8bd8ba79801e3
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-28 21:31:08 +0700

    fx fx command: add more handlers

--- vkreborn/handlers/general/fx/fx.py
+++ vkreborn/handlers/general/fx/fx.py
@@ -9,6 +9,12 @@ defaults = {"attachment": SUPPORTED_ATTACHMENTS, "blocking": False}
 
 
 @labeler.message(text="<_:prefix>fx <effects>", **defaults)
+@labeler.message(text="<_:prefix>фх <effects>", **defaults)
+@labeler.message(text="<_:prefix>фкс <effects>", **defaults)
+@labeler.message(text="<_:prefix>эфыкс <effects>", **defaults)
+@labeler.message(text="<_:prefix>фуфыкс <effects>", **defaults)
+@labeler.message(text="<_:prefix>эфых <effects>", **defaults)
+@labeler.message(text="<_:prefix>фуфых <effects>", **defaults)
 @error_handler.catch
 async def fx_handler(message: Message, effects: str):
     return await make(message, RawEffect(effects=effects))

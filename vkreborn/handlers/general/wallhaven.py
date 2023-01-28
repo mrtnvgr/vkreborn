17a7b313341a2140533cb98a27b760519888b64d
mrtnvgr <martynovegorOF@yandex.ru>
2023-01-28 20:09:05 +0700

    wallhaven command: add more aliases

--- vkreborn/handlers/general/wallhaven.py
+++ vkreborn/handlers/general/wallhaven.py
@@ -12,18 +12,70 @@ from vkreborn.vkbottle import labeler
 
 
 @labeler.message(text="<_:prefix>wh", blocking=False)
+@labeler.message(text="<_:prefix>вх", blocking=False)
+@labeler.message(text="<_:prefix>wallhaven", blocking=False)
+@labeler.message(text="<_:prefix>валлхавен", blocking=False)
+@labeler.message(text="<_:prefix>воллхавен", blocking=False)
+@labeler.message(text="<_:prefix>валхавен", blocking=False)
+@labeler.message(text="<_:prefix>волхавен", blocking=False)
+@labeler.message(text="<_:prefix>валлхавэн", blocking=False)
+@labeler.message(text="<_:prefix>воллхавэн", blocking=False)
+@labeler.message(text="<_:prefix>валхавэн", blocking=False)
+@labeler.message(text="<_:prefix>волхавэн", blocking=False)
+@labeler.message(text="<_:prefix>валлхавн", blocking=False)
+@labeler.message(text="<_:prefix>воллхавн", blocking=False)
+@labeler.message(text="<_:prefix>валхавн", blocking=False)
+@labeler.message(text="<_:prefix>волхавн", blocking=False)
 @error_handler.catch
 async def wh_noargs_handler(message: Message):
     return await send_random_single(message)
 
 
 @labeler.message(text="<_:prefix>wh <q>", blocking=False)
+@labeler.message(text="<_:prefix>вх <q>", blocking=False)
+@labeler.message(text="<_:prefix>wallhaven <q>", blocking=False)
+@labeler.message(text="<_:prefix>валлхавен <q>", blocking=False)
+@labeler.message(text="<_:prefix>воллхавен <q>", blocking=False)
+@labeler.message(text="<_:prefix>валхавен <q>", blocking=False)
+@labeler.message(text="<_:prefix>волхавен <q>", blocking=False)
+@labeler.message(text="<_:prefix>валлхавэн <q>", blocking=False)
+@labeler.message(text="<_:prefix>воллхавэн <q>", blocking=False)
+@labeler.message(text="<_:prefix>валхавэн <q>", blocking=False)
+@labeler.message(text="<_:prefix>волхавэн <q>", blocking=False)
+@labeler.message(text="<_:prefix>валлхавн <q>", blocking=False)
+@labeler.message(text="<_:prefix>воллхавн <q>", blocking=False)
+@labeler.message(text="<_:prefix>валхавн <q>", blocking=False)
+@labeler.message(text="<_:prefix>волхавн <q>", blocking=False)
 @error_handler.catch
 async def wh_query_handler(message: Message, q: str):
     return await send_random_single(message, q=q)
 
 
 @labeler.message(text="<_:prefix>wh <q> <categories:wh_switches>", blocking=False)
+@labeler.message(text="<_:prefix>вх <q> <categories:wh_switches>", blocking=False)
+@labeler.message(
+    text="<_:prefix>wallhaven <q> <categories:wh_switches>", blocking=False
+)
+@labeler.message(
+    text="<_:prefix>валлхавен <q> <categories:wh_switches>", blocking=False
+)
+@labeler.message(
+    text="<_:prefix>воллхавен <q> <categories:wh_switches>", blocking=False
+)
+@labeler.message(text="<_:prefix>валхавен <q> <categories:wh_switches>", blocking=False)
+@labeler.message(text="<_:prefix>волхавен <q> <categories:wh_switches>", blocking=False)
+@labeler.message(
+    text="<_:prefix>валлхавэн <q> <categories:wh_switches>", blocking=False
+)
+@labeler.message(
+    text="<_:prefix>воллхавэн <q> <categories:wh_switches>", blocking=False
+)
+@labeler.message(text="<_:prefix>валхавэн <q> <categories:wh_switches>", blocking=False)
+@labeler.message(text="<_:prefix>волхавэн <q> <categories:wh_switches>", blocking=False)
+@labeler.message(text="<_:prefix>валлхавн <q> <categories:wh_switches>", blocking=False)
+@labeler.message(text="<_:prefix>воллхавн <q> <categories:wh_switches>", blocking=False)
+@labeler.message(text="<_:prefix>валхавн <q> <categories:wh_switches>", blocking=False)
+@labeler.message(text="<_:prefix>волхавн <q> <categories:wh_switches>", blocking=False)
 @error_handler.catch
 async def wh_query_categories_handler(message: Message, q: str, categories: str):
     return await send_random_single(message, q=q, categories=categories)
@@ -31,6 +83,62 @@ async def wh_query_categories_handler(message: Message, q: str, categories: str)
 
 @labeler.message(
     text="<_:prefix>wh <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>вх <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>wallhaven <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>валлхавен <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>воллхавен <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>валхавен <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>волхавен <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>валлхавэн <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>воллхавэн <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>валхавэн <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>волхавэн <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>валлхавн <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>воллхавн <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>валхавн <q> <categories:wh_switches> <purity:wh_switches>",
+    blocking=False,
+)
+@labeler.message(
+    text="<_:prefix>волхавн <q> <categories:wh_switches> <purity:wh_switches>",
     blocking=False,
 )
 @error_handler.catch

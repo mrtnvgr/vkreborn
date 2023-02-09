749ed723c1b16b5cc9ac35daf2dde0f5d3fea5f0
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-09 19:17:18 +0700

    fixlayout command: add more use methods

--- vkreborn/handlers/general/fixlayout.py
+++ vkreborn/handlers/general/fixlayout.py
@@ -4,6 +4,8 @@ from vkreborn.error_handler import error_handler
 from vkreborn.rules import AliasRule
 from vkreborn.vkbottle import labeler
 
+ALIASES = ["fl", "фл", "fixlayout", "фикслайаут"]
+
 
 def translate(string: str):
     ru = """ёйцукенгшщзхъфывапролджэячсмитьбю.Ё"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,"""
@@ -15,8 +17,14 @@ def translate(string: str):
     return string.translate(change)
 
 
-@labeler.message(AliasRule(["fl", "фл", "fixlayout", "фикслайаут"]), reply=True)
+@labeler.message(AliasRule(ALIASES), reply=True)
 @error_handler.catch
-async def fixlayout_handler(message: Message):
+async def fixlayout_reply_handler(message: Message):
     text = message.reply_message.text
+    await message.reply(f'Перевод: "{translate(text)}"')
+
+
+@labeler.message(AliasRule(ALIASES, "<text>"))
+@error_handler.catch
+async def fixlayout_handler(message: Message, text: str):
     await message.reply(f'Перевод: "{translate(text)}"')

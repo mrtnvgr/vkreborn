0eabf9258322bd93d08ff50f0e836b0241c9de42
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-11 22:08:21 +0700

    fix automoder command

--- vkreborn/handlers/passive/automoder.py
+++ vkreborn/handlers/passive/automoder.py
@@ -9,6 +9,10 @@ from vkreborn.vkbottle import labeler
 @labeler.raw_event(UserEventType.CHAT_INFO_EDIT, dataclass=RawUserEvent)
 @error_handler.catch
 async def automoder_handler(event: RawUserEvent):
-    repo = UserRepository(user_id=event.object[3], chat_id=event.object[2])
+    # TODO: check if owner is admin here
+    # TODO: check if chat admins can invite people
+
+    chat_id = 2_000_000 - event.object[2]
+    repo = UserRepository(user_id=event.object[3], chat_id=chat_id)
     value = False if event.object[1] == 9 else True
     await repo.set_moder(value)

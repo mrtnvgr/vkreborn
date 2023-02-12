5974b4d9262a60325657d069a54a6617204e369d
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-12 16:57:39 +0700

    tinyurl command: add fallback to legacy api

--- vkreborn/handlers/general/tinyurl.py
+++ vkreborn/handlers/general/tinyurl.py
@@ -12,15 +12,18 @@ ALIASES = ["tinyurl", "тинуурл", "тиниурл", "тайниурл"]
 @labeler.message(AliasRule(ALIASES, "<url:url>"))
 @error_handler.catch
 async def tinyurl_handler(message: Message, url: str):
+    create_func = create_v2_api if TINYURL_API_TOKEN else create_legacy_api
+    await create_func(message, url)
+
+
+async def create_v2_api(message: Message, url: str):
     base_url = "https://api.tinyurl.com/create"
 
     payload = {
         "url": url,
+        "api_token": TINYURL_API_TOKEN,
         "domain": "rotf.lol",
     }
-
-    if TINYURL_API_TOKEN:
-        payload["api_token"] = TINYURL_API_TOKEN
 
     resp = await AiohttpClient().request_json(base_url, method="POST", params=payload)
 
@@ -28,3 +31,10 @@ async def tinyurl_handler(message: Message, url: str):
         await message.reply(resp["data"]["tiny_url"])
     else:
         await message.reply("\n".join(resp["errors"]))
+
+
+async def create_legacy_api(message: Message, url: str):
+    base_url = "http://tinyurl.com/api-create.php"
+    payload = {"url": url}
+    resp = await AiohttpClient().request_text(base_url, params=payload)
+    await message.reply(resp)

a3796483467212a64be83610290806faea302e82
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-12 15:36:30 +0700

    automoder command: remove todos

--- vkreborn/handlers/passive/automoder.py
+++ vkreborn/handlers/passive/automoder.py
@@ -9,10 +9,8 @@ from vkreborn.vkbottle import labeler
 @labeler.raw_event(UserEventType.CHAT_INFO_EDIT, dataclass=RawUserEvent)
 @error_handler.catch
 async def automoder_handler(event: RawUserEvent):
-    # TODO: check if owner is admin here
-    # TODO: check if chat admins can invite people
-
-    chat_id = 2_000_000 - event.object[2]
+    peer_id = event.object[2]
+    chat_id = 2_000_000 - peer_id
     repo = UserRepository(user_id=event.object[3], chat_id=chat_id)
     value = False if event.object[1] == 9 else True
     await repo.set_moder(value)

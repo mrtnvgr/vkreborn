69b45e5b1157172c60d3a60d27da5d7f94adbac5
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-19 15:37:03 +0500

    roll command: wrap replies

--- vkreborn/handlers/general/roll.py
+++ vkreborn/handlers/general/roll.py
@@ -13,21 +13,24 @@ ALIASES = ["roll", "rol", "ролл", "рол"]
 @error_handler.catch
 async def roll_default_handler(message: Message):
     response = random.randint(1, 100)
-    return await message.reply(response)
+    return await message.reply(wrap(response))
 
 
 @labeler.message(AliasRule(ALIASES, "<start:int>-<end:int>"))
 @error_handler.catch
 async def roll_ints_handler(message: Message, start: int, end: int):
     response = random.randint(start, end)
-    return await message.reply(response)
+    return await message.reply(wrap(response))
 
 
 @labeler.message(AliasRule(ALIASES, "<args:list>"))
 @error_handler.catch
 async def roll_strings_handler(message: Message, args: list):
     choice = random.choice(args)
-    return await message.reply(cleanup(choice))
+    return await message.reply(wrap(cleanup(choice)))
+
+
+wrap = lambda text: f'Результат: "{text}"'
 
 
 def cleanup(value: str):

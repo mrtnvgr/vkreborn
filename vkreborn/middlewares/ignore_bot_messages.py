6f4f016efa2bca294fc27731c5b7187655fe7878
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-22 20:12:39 +0700

    improve safety of ignore_bot_messages middleware

--- vkreborn/middlewares/ignore_bot_messages.py
+++ vkreborn/middlewares/ignore_bot_messages.py
@@ -4,5 +4,10 @@ from vkbottle.user import Message
 
 class IgnoreBotMessagesMiddleware(BaseMiddleware[Message]):
     async def pre(self):
-        if self.event.get_payload_json() == {"ignore": "me"}:
+        info = await self.event.ctx_api.users.get()
+        is_owner = info[0].id == self.event.from_id
+
+        ignore_payload = self.event.get_payload_json() == {"ignore": "me"}
+
+        if is_owner and ignore_payload:
             self.stop("Ignoring as requested...")

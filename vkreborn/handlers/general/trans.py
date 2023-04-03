d50af083d5d6b0f088cf39ad649c521a86b309bb
mrtnvgr <martynovegorOF@yandex.ru>
2023-04-03 16:51:59 +0700

    trans command: add custom "to" values

--- vkreborn/handlers/general/trans.py
+++ vkreborn/handlers/general/trans.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from async_google_trans_new import AsyncTranslator, TransError
 from vkbottle.user import Message
 
@@ -6,6 +8,7 @@ from vkreborn.rules import AliasRule
 from vkreborn.vkbottle import labeler
 
 ALIASES = ["trans", "транс", "translate", "перевод", "перевести", "translator", "переводчик"]
+DEFAULT_TO_ARG = "ru"
 
 
 @labeler.message(AliasRule(ALIASES), reply=True)
@@ -24,11 +27,28 @@ async def trans_to_handler(message: Message, to: str):
     return await message.reply(response)
 
 
-async def translate(content: str, to: str = "ru"):
+async def translate(content: str, to: Optional[str] = None):
     try:
+        to = expand_to_arg(to)
         translator = AsyncTranslator()
         response = await translator.translate(text=content, lang_tgt=to)
     except TransError as ex:
         return f"Произошла неожиданная ошибка!\n{ex.status_code}: {ex.reason}"
 
     return f'Перевод: "{response.strip()}"'
+
+
+def expand_to_arg(to: Optional[str] = None):
+    if not to:
+        return DEFAULT_TO_ARG
+
+    to = to.lower()
+    to_map = {
+        "ru": ("ру", "рус", "русский", "русский язык", "russian", "rus"),
+        "en": ("ен", "енг", "ин", "инг", "инглиш", "english", "eng"),
+        # TODO: japanesse, de, chinesse, ukranian, ...
+        # TODO: mention these custom values in docs
+    }
+    for key, values in to_map.items():
+        if to in values:
+            return key

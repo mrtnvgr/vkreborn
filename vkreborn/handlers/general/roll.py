19d055d115b2b5faf9265047a4cf6dd14938ff29
mrtnvgr <martynovegorOF@yandex.ru>
2023-04-11 21:41:14 +0700

    roll command: improve randint range handling

--- vkreborn/handlers/general/roll.py
+++ vkreborn/handlers/general/roll.py
@@ -12,15 +12,13 @@ ALIASES = ["roll", "rol", "ролл", "рол"]
 @labeler.message(AliasRule(ALIASES))
 @error_handler.catch
 async def roll_default_handler(message: Message):
-    response = random.randint(1, 100)
-    return await message.reply(wrap(response))
+    await randint(message, 1, 100)
 
 
 @labeler.message(AliasRule(ALIASES, "<start:int>-<end:int>"))
 @error_handler.catch
 async def roll_ints_handler(message: Message, start: int, end: int):
-    response = random.randint(start, end)
-    return await message.reply(wrap(response))
+    await randint(message, start, end)
 
 
 @labeler.message(AliasRule(ALIASES, "<args:list>"))
@@ -31,6 +29,18 @@ async def roll_strings_handler(message: Message, args: list):
 
 
 wrap = lambda text: f'Результат: "{text}"'
+
+
+async def randint(message: Message, start: int, end: int):
+    try:
+        response = random.randint(start, end)
+        await message.reply(wrap(response))
+    except ValueError:
+        await message.reply(
+            "Ошибка. Конечное число меньше начального."
+            "\n(Если вы хотели выбрать одно из чисел, a не из диапазона, то "
+            f'попробуйте "{start}, {end}", а не "{start}-{end}".)'
+        )
 
 
 def cleanup(value: str):
